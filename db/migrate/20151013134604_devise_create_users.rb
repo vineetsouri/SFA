09b2115f11e450bd69c23ae513eaafac09d0f992
vineetsouri <vineetsouri72@gmail.com>
2015-10-13 21:19:33 +0530

    Added omniauth gem and provided all callbacks

--- db/migrate/20151013134604_devise_create_users.rb
+++ db/migrate/20151013134604_devise_create_users.rb
@@ -3,11 +3,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      #t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      #t.string   :reset_password_token
-      #t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
